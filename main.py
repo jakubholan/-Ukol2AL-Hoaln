fff087382fafe7bb86e700fd23d40acf6828f1c4
jakubholan <159123546+jakubholan@users.noreply.github.com>
2024-11-20 09:30:07 +0000

    1/1

--- main.py
+++ main.py
@@ -9,3 +9,14 @@ serazeno = sorted(pole)
 print("Seřazené pole (standartní sort):", serazeno)
 
 #3. Přemýšlejte nad tím jak implementovat bubble sort.
+def bubble_sort(pole):
+    n = len(pole)
+    for i in range(n-1):
+        for j in range(0, n-i-1):
+            if pole[j] > pole[j+1]:
+                pole[j], pole[j+1] = pole[j+1], pole[j]
+            print(pole)
+    return pole
+    
+print(bubble_sort)
+
